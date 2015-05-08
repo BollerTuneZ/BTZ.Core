4bed7221000429ed78fd0c21c3d8154731a7e774
Jonas Home <ahlf@aiconsoft.de>
2015-05-08 17:06:36 +0200

    Log implementiert

--- BollerTuneZCore/Program.cs
+++ BollerTuneZCore/Program.cs
@@ -3,6 +3,11 @@ using System.Threading;
 using Testing;
 using Communication.Infrastructure;
 using Infrastructure;
+using log4net.Repository.Hierarchy;
+using log4net.Layout;
+using log4net.Appender;
+using log4net;
+using log4net.Core;
 
 
 namespace BollerTuneZCore
@@ -11,6 +16,7 @@ namespace BollerTuneZCore
 	{
 		public static void Main (string[] args)
 		{
+			Setup ();
 			BootStrapper.Run ();
 			ISteeringProcessor steeringProcessor = TinyIoC.TinyIoCContainer.Current.Resolve<ISteeringProcessor> ();
 			ISteeringConfigMessageProcessor steeringConfigMessageProcessor = TinyIoC.TinyIoCContainer.Current.Resolve<ISteeringConfigMessageProcessor> ();
@@ -32,5 +38,38 @@ namespace BollerTuneZCore
 			});
 */
 		}
+		public static void Setup()
+		{
+			Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+			PatternLayout patternLayout = new PatternLayout();
+			patternLayout.ConversionPattern = "%date [%thread] %-5level %logger - %message%newline";
+			patternLayout.ActivateOptions();
+
+			RollingFileAppender roller = new RollingFileAppender();
+			roller.AppendToFile = false;
+			roller.File = @"Logs\EventLog.txt";
+			roller.Layout = patternLayout;
+			roller.MaxSizeRollBackups = 5;
+			roller.MaximumFileSize = "1GB";
+			roller.RollingStyle = RollingFileAppender.RollingMode.Size;
+			roller.StaticLogFileName = true;            
+			roller.ActivateOptions();
+			hierarchy.Root.AddAppender(roller);
+
+			MemoryAppender memory = new MemoryAppender();
+			memory.ActivateOptions();
+			hierarchy.Root.AddAppender(memory);
+
+			ConsoleAppender consoleAppender = new ConsoleAppender {
+				Layout = patternLayout,
+			};
+			consoleAppender.ActivateOptions ();
+			hierarchy.Root.AddAppender (consoleAppender);
+
+			hierarchy.Root.Level = Level.Info;
+			hierarchy.Configured = true;
+		}
+
 	}
 }
